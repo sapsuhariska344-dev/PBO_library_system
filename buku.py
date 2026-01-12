3a546dd2a9647738892490dac62e668fb93447b1
Safitri Hafid_24107 <safitrihafid140126@gmail.com>
2026-01-12 20:14:16 +0900

    Update buku.py

--- buku.py
+++ buku.py
@@ -1,11 +1,17 @@
 class Buku:
-    def __init__(self, judul, penulis):
+    def __init__(self, kode, judul, penulis):
+        self.kode = kode
         self.judul = judul
         self.penulis = penulis
 
     def tampilkan(self):
-        print(f"judul: {self.judul}")
-        print(f"Penulis: {self .penulis}")
+        print(f"Kode Buku : {self.kode}")
+        print(f"Judul     : {self.judul}")
+        print(f"Penulis   : {self.penulis}")
+        print("-" * 30)
 
-buku1 = Buku("Python Dasar", "Deby")
-buku1 .tampilkan()
+buku1 = Buku("B001", "Pemrograman Python", "Guido van Rossum")
+buku2 = Buku("B002", "Struktur Data", "Mark Allen Weiss")
+
+buku1.tampilkan()
+buku2.tampilkan()
